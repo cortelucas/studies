cd2527204efcda4c0abe9e8cef9bb4a6d2298753
Lucas Corte <corte1994.lc@gmail.com>
2022-04-14 13:26:07 -0300

    implement bootstrap

---
+++ fatec/php/Calculator/bootstrap.php
@@ -0,0 +1,23 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+$method = $_SERVER['REQUEST_METHOD'];
+$path = $_SERVER['PATH_INFO'] ?? "/";
+
+$route = new \App\Router($method, $path);
+
+$route->get("/login", "App\Controllers\LoginController::index");
+
+$route->get("/", "App\Controllers\CalculadoraController::index");
+
+$result = $route->handler();
+if (!$result) {
+  http_response_code(404);
+  echo "Página não encontrada!";
+  die();
+}
+
+echo $result($route->getParams());
+
+//php -S localhost:8000 -t public/
