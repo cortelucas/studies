db83575ffd3d8e36c3f9d3846625630af1fa7991
Lucas Corte <corte1994.lc@gmail.com>
2022-04-14 08:04:45 -0300

    exe 02 python

---
+++ python/guanabara/world_01/challenges/challenge_02.py
@@ -0,0 +1,7 @@
+print("----- Desafio 02 -----")
+
+day: int = input("Qual o dia do seu nascimento? ")
+month: str | int = input("Qual o mês do seu nascimento? ")
+year: int = input("Qual o ano do seu nascimento? ")
+
+print(f"Você nasceu no dia {day} de {month} de {year}. Correto?")
