2b37feaa085ed7a8b8fdeb02fc093933ad3bbdfa
Lucas Corte <corte1994.lc@gmail.com>
2022-04-14 09:03:22 -0300

    aula 6

---
+++ python/guanabara/world_01/lessons/lesson_06a.py
@@ -0,0 +1,11 @@
+number_01: int = 123
+print(number_01)
+print(type(number_01))
+
+any_number: float = 7.5
+print(any_number)
+print(type(any_number))
+
+message: str = "Olá mundo!"
+print(message)
+print(type(message))
